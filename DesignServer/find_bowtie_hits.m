56aeddb29c49486d13bb8904aad3a965ba908f55
Arjun Raj <arjunrajlab@gmail.com>
2017-10-25 14:29:01 -0400

    Changed IP address for probe design server due to move.

--- DesignServer/find_bowtie_hits.m
+++ DesignServer/find_bowtie_hits.m
@@ -11,7 +11,8 @@ function hts = find_bowtie_hits(inseq,merlength,database)
 dom = xmlread('sample_bowtie_search.txt');
 
 %endpoint = 'http://192.168.1.74:8000/service';
-endpoint = 'http://158.130.146.251:8000/service';
+%endpoint = 'http://158.130.146.251:8000/service';
+endpoint = 'http://158.130.14.49:8000/service';
 
 ch = dom.getElementsByTagName('bow:mer_length');
 it = ch.item(0);
